1c83339e0a2ea707dc3d0d008a145e378a4d4ca5
MrRonak <113142991+MrRonak2@users.noreply.github.com>
2022-11-09 19:56:11 -0300

    Update LoaderV2.lua

--- LoaderV2.lua
+++ LoaderV2.lua
@@ -59,7 +59,7 @@ Icon.Image = "rbxassetid://7072725342"
 
 UIAspectRatioConstraint.Parent = Icon
 
-local function HEGVC_fake_script() -- check.check_script 
+local function BJOJV_fake_script() -- check.check_script 
 	local script = Instance.new('LocalScript', check)
 
 	function exploitable()
@@ -68,11 +68,9 @@ local function HEGVC_fake_script() -- check.check_script
 		for _,v in pairs(replicated:GetChildren()) do
 			if v:IsA("RemoteEvent") then
 				if v.Name == "DeleteCar" then
-					script.Parent.TextColor3 = Color3.fromRGB(48, 255, 72)
 					script.Parent.Text= "Exploitable"
 					break
 				else
-					script.Parent.TextColor3 = Color3.fromRGB(255, 0, 4)
 					script.Parent.Text= "Is Not Exploitable"
 				end
 			end
@@ -96,19 +94,35 @@ local function HEGVC_fake_script() -- check.check_script
 	exploitable()
 	
 	if script.Parent.Text == "Exploitable" then
-		script.Parent.Text = "Remote Event Found"
-		wait(1)
+		local textlabel = script.Parent
+		function typewrite(object,text)
+			for i = 1, #text,1 do
+				object.Text = string.sub(text,1,i)
+				wait(.05)
+			end
+		end
+		script.Parent.TextColor3 = Color3.fromRGB(48, 255, 72)
+		typewrite(textlabel,"Remote Event Found")
+		wait(1.9)
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/MrRonak2/DestroyerX/main/ScriptV2.lua",true))()
 		script.Parent.Parent.Parent:Destroy()
 	else
-		script.Parent.Text = "Remote Event Not Found"
-		wait(1)
+		local textlabel = script.Parent
+		function typewrite(object,text)
+			for i = 1, #text,1 do
+				object.Text = string.sub(text,1,i)
+				wait(.05)
+			end
+		end
+		script.Parent.TextColor3 = Color3.fromRGB(255, 0, 4)
+		typewrite(textlabel,"Remote Event Not Found")
+		wait(1.9)
 		script.Parent.Parent.Parent:Destroy()
 	end
 	
 end
-coroutine.wrap(HEGVC_fake_script)()
-local function APIGU_fake_script() -- Icon.exit_script 
+coroutine.wrap(BJOJV_fake_script)()
+local function BNPMT_fake_script() -- Icon.exit_script 
 	local script = Instance.new('LocalScript', Icon)
 
 	local loader = script.Parent.Parent.Parent
@@ -118,4 +132,10 @@ local function APIGU_fake_script() -- Icon.exit_script
 	
 	script.Parent.MouseButton1Click:Connect(exit)
 end
-coroutine.wrap(APIGU_fake_script)()
+coroutine.wrap(BNPMT_fake_script)()
+local function QKCSUZ_fake_script() -- main.LocalScript 
+	local script = Instance.new('LocalScript', main)
+
+	
+end
+coroutine.wrap(QKCSUZ_fake_script)()
